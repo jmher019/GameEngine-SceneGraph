f1285d937de39c1f1c722b8a3325198d55eee9dc
Jonathan Hernandez <jmher019@gmail.com>
2018-11-24 19:28:03 -0800

    Scene graph now needs information about perspective and the current camera

--- src/include/SceneGraph.hpp
+++ src/include/SceneGraph.hpp
@@ -1,23 +1,52 @@
 #ifndef SCENE_GRAPH_HPP
 #define SCENE_GRAPH_HPP
 
-#include <SceneObject.hpp>
-#include <SceneUtils.hpp>
+#include <Camera.hpp>
 
 class SceneGraph {
 protected:
     shared_ptr<SceneObject> root = nullptr;
+    shared_ptr<Camera> camera = nullptr;
+    float fov = 45.f;
+    float width = 512.f;
+    float height = 512.f;
+    float nearZ = 0.1f;
+    float farZ = 100.f;
 
     shared_ptr<SceneObject> getSceneObject(const string& name, const shared_ptr<SceneObject>& sceneObject) const noexcept;
 
 public:
     SceneGraph(const shared_ptr<SceneObject>& root = make_shared<SceneObject>(string("World_") + SceneUtils::createId()));
 
-    virtual void draw(const mat4& ProjectionViewMatrix) const noexcept;
+    virtual void draw(void) const noexcept;
 
     shared_ptr<SceneObject> getSceneObject(const string& name) const noexcept;
 
     const shared_ptr<SceneObject>& getRoot(void) const noexcept;
+
+    const shared_ptr<Camera>& getCamera(void) const noexcept;
+
+    const float& getFov(void) const noexcept;
+
+    const float& getWidth(void) const noexcept;
+
+    const float& getHeight(void) const noexcept;
+
+    const float& getNearZ(void) const noexcept;
+
+    const float& getFarZ(void) const noexcept;
+
+    void setCamera(const shared_ptr<Camera>& camera) noexcept;
+
+    void setFov(const float& fov) noexcept;
+
+    void setWidth(const float& width) noexcept;
+
+    void setHeight(const float& height) noexcept;
+
+    void setNearZ(const float& nearZ) noexcept;
+
+    void setFarZ(const float& farZ) noexcept;
 };
 
 #endif // !SCENE_GRAPH_HPP
--- src/sources/SceneGraph.cpp
+++ src/sources/SceneGraph.cpp
@@ -4,8 +4,10 @@ SceneGraph::SceneGraph(const shared_ptr<SceneObject>& root):
     root(root) {
 }
 
-void SceneGraph::draw(const mat4& ProjectionViewMatrix) const noexcept {
-    root->draw(ProjectionViewMatrix);
+void SceneGraph::draw() const noexcept {
+    if (camera != nullptr) {
+        root->draw(perspectiveFov(fov, width, height, nearZ, farZ) * camera->getCameraMatrix());
+    }
 }
 
 shared_ptr<SceneObject> SceneGraph::getSceneObject(const string& name) const noexcept {
@@ -30,4 +32,52 @@ shared_ptr<SceneObject> SceneGraph::getSceneObject(const string& name, const sha
 
 const shared_ptr<SceneObject>& SceneGraph::getRoot(void) const noexcept {
     return root;
+}
+
+const shared_ptr<Camera>& SceneGraph::getCamera(void) const noexcept {
+    return camera;
+}
+
+const float& SceneGraph::getFov(void) const noexcept {
+    return fov;
+}
+
+const float& SceneGraph::getWidth(void) const noexcept {
+    return width;
+}
+
+const float& SceneGraph::getHeight(void) const noexcept {
+    return height;
+}
+
+const float& SceneGraph::getNearZ(void) const noexcept {
+    return nearZ;
+}
+
+const float& SceneGraph::getFarZ(void) const noexcept {
+    return farZ;
+}
+
+void SceneGraph::setCamera(const shared_ptr<Camera>& camera) noexcept {
+    this->camera = camera;
+}
+
+void SceneGraph::setFov(const float& fov) noexcept {
+    this->fov = fov;
+}
+
+void SceneGraph::setWidth(const float& width) noexcept {
+    this->width = width;
+}
+
+void SceneGraph::setHeight(const float& height) noexcept {
+    this->height = height;
+}
+
+void SceneGraph::setNearZ(const float& nearZ) noexcept {
+    this->nearZ = nearZ;
+}
+
+void SceneGraph::setFarZ(const float& farZ) noexcept {
+    this->farZ = farZ;
 }
